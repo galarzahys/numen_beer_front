64ab7b686ec926ffa801dca5dca787f3d81db5d3
galarzahys <galarza.hys@gmail.com>
2022-09-05 13:21:42 -0300

    CSS modified

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -80,6 +80,7 @@ const ContactForm = () => {
           })}
           onSubmit={(values, { setSubmitting }) => {
             setTimeout(() => {
+              
               alert(JSON.stringify(values, null, 2));
               setSubmitting(false);
             }, 400);
