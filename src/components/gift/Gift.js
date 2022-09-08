9dca53122e4f7297f790bef4f5f83700f8bf27d5
Lucas <lucastrento86@gmail.com>
2022-09-08 04:06:06 -0300

    gift card terminada

--- src/components/gift/Gift.js
+++ src/components/gift/Gift.js
@@ -1,24 +1,31 @@
+import Button from "../button/Button"
 import "./gift.css"
+
 
 
 const Gift = () => {
   return (
-    <div className="gift_container">
+    <>
+      <div className="gift_container">
         <div className="text_container">
-            <h2>sorprende a alguien trabajando desde casa</h2>
-            <p>
-                ¿Tiene un colega o familiar que trabaja duro en casa?
-                Nuestra caja de bocadillos y bebidas para trabajar desde casa es <br />
-                la manera perfecta de poner una sonrisa en su rostro, entregada directamente <br />
-                en su puerta.
-                para comprar para un grupo de personas, póngase en contacto y podemos discutir <br />
-                paquetes a medida y mensajes personalizados
-            </p>
+          <h2>GIFT CARD </h2>
+          <p>
+            <span>
+              Si no sabés que regalar, <br />
+              ¡esta Gift Card es la solución!<br /><br />
+            </span>
+            La manera perfecta de poner una sonrisa en un rostro,<br /> 
+            es regalar unas unas buenas Numen Beer!!!.Entregada directamente en su puerta.<br />
+            Para comprar para un grupo de personas, póngase en contacto <br /> y podemos discutir 
+            paquetes a medida <br />y mensajes personalizados.
+          </p>
+          <Button text="Comprar Ahora" className="primary" url='e-Shop'/>
         </div>
         <div className="img_container">
             <img src="./images/oferta.jpg" alt="cerveza" />
         </div>
-    </div>
+      </div>
+    </>
   )
 }
 
