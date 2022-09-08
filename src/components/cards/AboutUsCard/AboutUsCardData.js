5db754d15659870f9ee68b15ab3ce81ed123b063
Lucas <lucastrento86@gmail.com>
2022-09-08 00:53:01 -0300

    complete mi tarjeta seccion nosotros

--- src/components/cards/AboutUsCard/AboutUsCardData.js
+++ src/components/cards/AboutUsCard/AboutUsCardData.js
@@ -10,10 +10,10 @@ export const AboutUsCardData = [
     },
     {
         developer_id: 2,
-        developer_img:"developer_1",
-        developer_Name: "Morales Santiago",
-        developer_age: 26,
-        description:"Estudiante de la diplomatura Desarrollo Web Full Stack en Academia Numen.",
+        developer_img:"developer_2",
+        developer_Name: "Trento Lucas",
+        developer_age: 35,
+        description:"Tecnico Superior en Desarrollo de Software y Estudiante de la diplomatura Desarrollo Web Full Stack en Academia Numen.",
         social_networks:"./Contacto"
 
     },
